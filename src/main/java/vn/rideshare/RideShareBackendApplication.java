a07b136ee2274b146a5b99d73a33cf3d6efe427d
Phạm Trung Hiếu <phamtrunghieu6d@gmail.com>
2022-09-12 00:00:03 +0700

    test deploy heroku

--- src/main/java/vn/rideshare/RideShareBackendApplication.java
+++ src/main/java/vn/rideshare/RideShareBackendApplication.java
@@ -17,10 +17,6 @@ import vn.rideshare.shared.CustomMongoTemplate;
 @EnableScheduling
 @SpringBootApplication
 public class RideShareBackendApplication {
-
-    @Value("${rt-server.host}")
-    private String host;
-    
     @Value("${rt-server.port}")
     private Integer port;
 
